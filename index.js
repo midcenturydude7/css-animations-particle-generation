356ce2816b02a212814611e3f401866252701ccf
midCenturyDude7 <mgriffes@gmail.com>
2025-09-28 21:39:22 -0700

    Updated files

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import { random, range } from "lodash";
-import "/styles.css"
+import "./styles.css";
+
+const btn = document.querySelector(".particleButton");
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("liked");
+
+  const isLiked = btn.classList.contains("liked");
+
+  if (!isLiked) return;
+
+  range(5).forEach(() => {
+    const particle = document.createElement(span);
+    particle.classList.add("particle");
+
+    particle.style.top = random(0, 100) + "%";
+    particle.style.left = random(0, 100) + "%";
+
+    btn.appendChild(particle);
+  });
+});
