8767e1f70146d6940904e6a2f13705563e7a2129
midCenturyDude7 <mgriffes@gmail.com>
2025-09-28 22:08:51 -0700

    Updated dependencies and resolved bug

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 import { random, range } from "lodash";
-import "./styles.css";
 
 const btn = document.querySelector(".particleButton");
 
+console.log("Script loaded, button found:", btn);
+
 btn.addEventListener("click", () => {
+  console.log("Button clicked!");
   btn.classList.toggle("liked");
 
   const isLiked = btn.classList.contains("liked");
+  console.log("Is liked:", isLiked);
 
   if (!isLiked) return;
 
-  range(5).forEach(() => {
-    const particle = document.createElement(span);
+  range(5).forEach((i) => {
+    console.log(`Creating particle ${i + 1}`);
+    const particle = document.createElement("span");
     particle.classList.add("particle");
 
-    particle.style.top = random(0, 100) + "%";
-    particle.style.left = random(0, 100) + "%";
+    const top = random(0, 100);
+    const left = random(0, 100);
+    console.log(`Particle position: top ${top}%, left ${left}%`);
+    
+    particle.style.top = top + "%";
+    particle.style.left = left + "%";
 
     btn.appendChild(particle);
+    console.log("Particle added to DOM", particle);
+    
+    // Remove particle after animation completes
+    setTimeout(() => {
+      particle.remove();
+      console.log("Particle removed");
+    }, 1000);
   });
 });
