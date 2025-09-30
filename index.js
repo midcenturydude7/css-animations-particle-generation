f9eb607f67ecd82d411145711bd058c87b745031
midCenturyDude7 <mgriffes@gmail.com>
2025-09-29 20:21:31 -0700

    Minor edit

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ btn.addEventListener("click", () => {
     const top = random(0, 100);
     const left = random(0, 100);
     console.log(`Particle position: top ${top}%, left ${left}%`);
-    
+
     particle.style.top = top + "%";
     particle.style.left = left + "%";
 
     btn.appendChild(particle);
     console.log("Particle added to DOM", particle);
-    
+
     // Remove particle after animation completes
     setTimeout(() => {
       particle.remove();
